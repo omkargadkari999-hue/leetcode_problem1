736cb64d824dec74d231f217668fcbf107a92538
omkar gadkari <omkargadkari999@gmail.com>
2026-08-17 07:44:24 +0530

    Add Easy Python Solution from LeetCode

--- LeetCode/Python/Easy/1. Two Sum/Solution.py
+++ LeetCode/Python/Easy/1. Two Sum/Solution.py
@@ -1,9 +1,9 @@
 class Solution:
     def twoSum(self, nums, target):
-        
+        n =len(nums)
         dicto={}
         for i in range(0,n):
-            diff = target - n
+            diff = target - nums[i]
             if diff in dicto:
                 return [dicto[diff],i]
             dicto[n]=i
