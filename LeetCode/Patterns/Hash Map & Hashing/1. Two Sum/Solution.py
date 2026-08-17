6e0a09ffb746ef195c17325027a3d4fe02ad5020
omkar gadkari <omkargadkari999@gmail.com>
2026-08-17 07:21:47 +0530

    Add Python Solution to Patterns/Hash Map & Hashing

---
+++ LeetCode/Patterns/Hash Map & Hashing/1. Two Sum/Solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums, target):
+        hashmap = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+
+            if complement in hashmap:
+                return [hashmap[complement], i]
+
+            hashmap[num] = i
+
+
+        
+        
